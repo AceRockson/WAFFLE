d850c4dbc91ca9197d8dbb2bb5eb5f526ea0cf0f
Dimitrios Gkizanis <acerockson@hotmail.com>
2016-04-11 04:10:58 +0300

    Update WAFFLE

--- waffle.php
+++ waffle.php
@@ -1,116 +1,26 @@
 <?php
 
-define( 'WAF_NAME', 'WAFFLE' );
-define( 'CACHE_DIR', '/tmp/' );
-
-/* Features */
-define( 'MAX_INPUT_QUERY_STRING', 255 );
-define( 'ONLY_HTTPS', false );
-define( 'MAX_INPUT_ELEMENTS', 30 );
-define( 'MAX_INPUT_VAR', 0 ); //0 to disable
-define( 'NULL_BYTE_PROTECTION', true );
-define( 'COMMAND_INJECTION_FUNC_BASED', false );
-define( 'PROTECT_LFI', true );
-define( 'PROTECT_RFI', true );
-define( 'PROTECT_PROXIES', true );
-define( 'PROTECT_TOR', true );
-
-define( 'PROTECT_FLOOD', true );
-define( 'FLOOD_LIMIT', 60 );
-define( 'FREQUENCY_SECONDS', 2 );
-define( 'MAX_MINUTES_BAN', 10 );
-
-########################################################################################
-/* Bash Functions */
-$bad_functions = array(
-    'shell_exec',
-    'passthru',
-    'popen',
-    'proc_open',
-    'exec',
-    'system' );
-
-/*Proxy Headers */
-$proxy_headers = array(
-    'HTTP_VIA',
-    'HTTP_X_FORWARDED_FOR',
-    'HTTP_FORWARDED_FOR',
-    'HTTP_X_FORWARDED',
-    'HTTP_FORWARDED',
-    'HTTP_CLIENT_IP',
-    'HTTP_FORWARDED_FOR_IP',
-    'VIA',
-    'X_FORWARDED_FOR',
-    'FORWARDED_FOR',
-    'X_FORWARDED',
-    'FORWARDED',
-    'CLIENT_IP',
-    'FORWARDED_FOR_IP',
-    'HTTP_PROXY_CONNECTION' );
-
-$allowed_methods = array( 'GET', 'POST' );
-
-/*Regular Expressions for SQL and Command Injections*/
-$command_injection = "\;.*|\|.*|" . PHP_EOL . '.*';
-$sql = "[\x22\x27](\s)*(or|and)(\s).*(\s)*\x3d|cmd=ls|cmd%3Dls|(drop|alter|create|truncate).*(index|table|database)|insert(\s).*(into|member.|value.)|(select|union|order).*(select|union|order)|0x[0-9a-f][0-9a-f]|sleep\(|\-\-|\/\*|\/\/|benchmark\([0-9]+,[a-z]+|eval\(.*\(.*|update.*set.*=|delete.*from";
+$config = parse_ini_file( "config.ini", true ) or die( 'Config.ini Parse Fail' );
+define( 'CACHE_DIR', $config['main']['cache_dir'] );
+$allowed_methods = array_map( 'trim', explode( ',', $config['input']['allowed_methods'] ) );
+$proxies_headers = ( $config['proxies']['proxies_protection'] ) ? array_map( 'trim', explode( ',', $config['proxies']['proxies_headers'] ) ) : array();
+$bad_functions = ( $config['bad_funcs']['bad_func_enable'] ) ? array_map( 'trim', explode( ',', $config['bad_funcs']['bad_functions'] ) ) : array();
 
 $user_ip = $_SERVER['REMOTE_ADDR'];
+$query_string = urldecode( $_SERVER['QUERY_STRING'] );
 
-/* Flood Protection */
-if ( PROTECT_FLOOD )
+
+if ( $config['protections']['user_agent_protection'] )
 {
-    $user_file = CACHE_DIR . $user_ip;
+    if ( $config['user_agents']['block_empty_ua'] && empty( $_SERVER['HTTP_USER_AGENT'] ) )
+        attack_found( "EMPTY USER AGENT" );
 
-    if ( file_exists( $user_file ) )
-    {
-        $flood_row = json_decode( file_get_contents( $user_file ), true );
-
-        if ( $flood_row['banned'] && time() - $flood_row['banned_time'] <= MAX_MINUTES_BAN * 60 )
-        {
-            http_response_code( 404 );
-            exit;
-        }
-
-        if ( time() - $flood_row['last_request'] <= FREQUENCY_SECONDS )
-        {
-            ++$flood_row['requests'];
-            if ( $flood_row['requests'] >= FLOOD_LIMIT )
-            {
-                $flood_row['banned'] = true;
-                $flood_row['banned_time'] = time();
-            }
-            $flood_row['last_request'] = time();
-            file_put_contents( $user_file, json_encode( $flood_row ), LOCK_EX );
-        }
-        else
-        {
-            $flood_row['requests'] = 0;
-            $flood_row['banned'] = false;
-            $flood_row['banned_time'] = 0;
-            $flood_row['last_request'] = time();
-            file_put_contents( $user_file, json_encode( $flood_row ), LOCK_EX );
-        }
-    }
-    else
-        file_put_contents( $user_file, json_encode( array(
-            'banned_time' => 0,
-            'banned' => false,
-            'requests' => 0,
-            'last_request' => time() ) ), LOCK_EX );
+    if ( preg_match( "/^(" . $config['user_agents']['user_agents'] . ").*/i", $_SERVER['HTTP_USER_AGENT'], $matched ) )
+        attack_found( "BAD USER AGENT({$_SERVER['HTTP_USER_AGENT']})" );
 }
 
-/* Proxy Access Prtoection */
-if ( PROTECT_PROXIES )
-{
-    foreach ( $proxy_headers as $x )
-    {
-        if ( !empty( $_SERVER[$x] ) )
-            die( "PROXIES NOT ALLOWED" );
-    }
-}
 
-/* TOR Access Protection */ 
- if ( PROTECT_TOR )
+if ( $config['proxies']['tor_protection'] && is_writeable( CACHE_DIR ) )
 {
     if ( !file_exists( CACHE_DIR . 'tor_exit_nodes' ) || time() - filemtime( CACHE_DIR . 'tor_exit_nodes' ) >= 1800 )
     {
@@ -123,123 +33,89 @@ if ( PROTECT_PROXIES )
     else
         $ips = array_map( 'trim', file( CACHE_DIR . 'tor_exit_nodes' ) );
 
-
     if ( in_array( $user_ip, $ips ) )
-    {
-        exit( "TOR FOUND!" );
-    }
+        attack_found( "TOR FOUND ON $user_ip" );
 }
 
-/* Query String Size Protection */
-$query_string = urldecode( $_SERVER['QUERY_STRING'] );
-
-if ( strlen( $query_string ) >= MAX_INPUT_QUERY_STRING )
+/* Proxy Access Prtoection */
+foreach ( $proxies_headers as $x )
 {
-    exit( "MAX INPUT REACHED!" );
+    if ( !empty( $_SERVER[$x] ) )
+        attack_found( "PROXIES NOT ALLOWED ($x is SET on \$_SERVER)" );
 }
+
+if ( strlen( $query_string ) >= $config['input']['query_string_max'] )
+    attack_found( "MAX INPUT REACHED! (QUERY STRING: $query_string)" );
 
 /* Method Request */
 if ( !in_array( $_SERVER['REQUEST_METHOD'], $allowed_methods ) )
-{
-    exit( "METHOD NOT ALLOWED!" );
-}
+    attack_found( "METHOD ({$_SERVER['REQUEST_METHOD']}) NOT ALLOWED" );
 
 /* HTTPS Limitation */
-if ( ONLY_HTTPS && $_SERVER['REQUEST_SCHEME'] != 'https' )
+if ( $config['https']['use_only_https'] && $_SERVER['REQUEST_SCHEME'] != 'https' )
 {
-    exit( "NO HTTPS SCHEME FOUND!" );
+
+    if ( $config['https']['redirect'] )
+    {
+        header( 'Location: https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'], true, 301 );
+        exit;
+    }
+
+    attack_found( "NO HTTPS SCHEME FOUND" );
 }
 
 /* Local File Inclusion Protection */
-if ( PROTECT_LFI && ( stristr( $query_string, '/' ) or stristr( $query_string, '\\' ) ) )
-{
-    exit( "LFI ATTEMPT PREVENTED!" );
-}
+if ( $config['protections']['lfi_protection'] && ( stristr( $query_string, '/' ) or stristr( $query_string, '\\' ) ) )
+    attack_found( "LFI ATTEMPT PREVENTED" );
+
 /* Remote File Inclusion Protection */
-if ( PROTECT_RFI && stristr( $query_string, 'http:' ) )
-{
-    exit( "RFI ATTEMPT PREVENTED!" );
-}
+if ( $config['protections']['rfi_protection'] && stristr( $query_string, 'http' ) )
+    attack_found( "RFI ATTEMPT PREVENTED" );
 
-/* Command Injection Protection */
-$check_for_command_injection = true;
-if ( COMMAND_INJECTION_FUNC_BASED )
+foreach ( $bad_functions as $bad_func )
 {
-    $all_func_disabled = true;
-    foreach ( $bad_functions as $bad_func )
+    if ( function_exists( $bad_func ) && is_callable( $bad_func ) )
     {
-        if ( function_exists( $bad_func ) && is_callable( $bad_func ) )
-        {
-            $all_func_disabled = false;
-            exit( "Function <b>$bad_func</b> is enabled. " . WAF_NAME . " can't run with these functions enabled as it is a security risk. Request terminated!" );
-        }
+        exit( "Function <b>$bad_func</b> is enabled. WAFFLE can't run with these functions enabled as it is a security risk. Request terminated!" );
     }
-
-    if ( !$all_func_disabled )
-        $check_for_command_injection = false;
 }
 
 $WAF_array = array(
     '_GET' => &$_GET,
     '_POST' => &$_POST,
-    '_REQUEST' => &$_REQUEST,
-    '_COOKIE' => &$_COOKIE,
-    '_SESSION' => &$_SESSION );
-
+    '_REQUEST' => &$_REQUEST );
 
 foreach ( $WAF_array as $global_v => $elements )
 {
-    if ( count( $elements ) > MAX_INPUT_ELEMENTS )
-    {
-        exit( "MAX INPUT VARS REACHED!" );
-    }
-
-    $$global_v = my_array_map( "htmlentities", $elements );//XSS Protection
+    if ( count( $elements ) > $config['input']['max_input_elements'] )
+        attack_found( "MAX INPUT VARS ON $global_v  ARRAY REACHED!" );
 
     foreach ( $$global_v as $k => $v )
     {
         $v = urldecode( $v );
 
-        if ( MAX_INPUT_VAR != 0 && strlen( $v ) > MAX_INPUT_VAR )
-        {
-            exit( "MAX INPUT ON VAR REACHED!" );
-        }
+        if ( $config['input']['max_strlen_var'] != 0 && strlen( $v ) > $config['input']['max_strlen_var'] )
+            attack_found( "MAX INPUT ON VAR ($k) -> $global_v ARRAY REACHED!" );
 
-        if ( NULL_BYTE_PROTECTION && stristr( $v, '\0' ) )
-        {
-            ${$global_v}
-            {
-                $k}
-            = str_ireplace( '\0', '', $v );//Null Byte Pr
-        }
+        if ( $config['protections']['null_byte_protection'] && stristr( $v, '\0' ) )
+            attack_found( 'NULL BYTE' );
 
-        if ( $check_for_command_injection )
-        {
-            if ( preg_match( "/^.*(" . $command_injection . ").*/i", $v, $matched ) )
-            {
-                ${$global_v}
-                {$k} = str_ireplace( $matched[1], '', $v );//Command Injection Protection
-            }
-        }
+        if ( $config['protections']['enable_command_injection_protection'] )
+            if ( preg_match( "/^.*(" . implode( '|', $config['rules']['command_injection'] ) . ").*/i", $v, $matched ) )
+                attack_found( "COMMAND INJECTION: " . $matched[1] );
 
+        if ( $config['protections']['enable_sqli_protection'] )
+            if ( preg_match( "/^.*(" . implode( '|', $config['rules']['sqli'] ) . ").*/i", $v, $matched ) )
+                attack_found( "SQL INJECTION: " . $matched[1] );
 
-        if ( preg_match( "/^.*(" . $sql . ").*/i", $v, $matched ) )
-        {
-            ${$global_v}
-            {$k} = str_ireplace( $matched[1], '', $v );//SQL Injection Protection
-        }
+        if ( $config['protections']['enable_xss_protection'] )
+            if ( preg_match( "/^.*(" . implode( '|', $config['rules']['xss'] ) . ").*/i", $v, $matched ) )
+                attack_found( "XSS: " . $matched[1] );
     }
 }
 
-function my_array_map( $function, $arr )
+function attack_found( $match )
 {
-    $result = array();
-    foreach ( $arr as $key => $val )
-    {
-        $result[$key] = ( is_array( $val ) ? my_array_map( $function, $val ) : $function( $val ) );
-    }
-
-    return $result;
+    file_put_contents( CACHE_DIR . 'attacks.txt', "[WARNING] Possible Attack Found & Eliminated (  =>  '" . str_replace( "\n", "\\n", $match ) . "'  <=  ) @ " . date( "F j, Y, g:i a" ) . "\n", FILE_APPEND );
+    exit( 'Hacking Attempt Detected & Eliminated' );
 }
-
-?>
